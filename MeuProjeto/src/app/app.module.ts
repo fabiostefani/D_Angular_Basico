c418455326c3f73696419bcd64f729f10a881990
Fabio de Stefani <fabiostefani@gmail.com>
2019-11-15 07:27:03 -0300

    Estrutura navegação aplicação

--- MeuProjeto/src/app/app.module.ts
+++ MeuProjeto/src/app/app.module.ts
@@ -2,10 +2,16 @@ import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
 
 import { AppComponent } from './app.component';
+import { MenuComponent } from './navegacao/menu/menu.component';
+import { HomeComponent } from './navegacao/home/home.component';
+import { FooterComponent } from './navegacao/footer/footer.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    MenuComponent,
+    HomeComponent,
+    FooterComponent
   ],
   imports: [
     BrowserModule
